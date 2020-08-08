634a2d9d736e82653d3db0907024aea79686af2e
Hyper-KVM <hyperkvmx86@gmail.com>
2020-08-08 16:14:28 -0400

    missed one

--- fetch_cord/run-rpc.py
+++ fetch_cord/run-rpc.py
@@ -44,7 +44,7 @@ def cycle0():
             "assets": {
                 "small_text": desktopid, #this will show de/wm name and de/wm version
                 "small_image": desktopid, #this shows the de/wm logo
-                "large_text": osline[0], #shows distro version and name on hover (refence to pretty name in /etc/os-release)
+                "large_text": sysosline[0], #shows distro version and name on hover (refence to pretty name in /etc/os-release)
                 "large_image": "big" #this will be the distro logo
             }
         }
