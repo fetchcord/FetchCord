9db8b1d7cbded2abfa595c54a2fab8f3896e5f76
MrPotatoBobx <63403023+MrPotatoBobx@users.noreply.github.com>
2020-08-14 19:17:17 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name='FetchCord',
-    version=2.2,
+    version=2.3,
     description='grabs information about your Distro and displays it as Discord Rich Presence.',
     long_description=open(
         join(dirname(__file__), 'readme.md')).read(),
