92ac109a06c4fac4fe462336684d24c0370cd4b3
Hyper-KVM <hyperkvmx86@gmail.com>
2020-08-28 20:49:42 -0400

    Version bump 2.5.5 > 2.6.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name='FetchCord',
-    version='2.5.5',
+    version='2.6.0',
     description='grabs information about your Distro and displays it as Discord Rich Presence.',
     long_description=open(
         join(dirname(__file__), 'README.md')).read(),
