c2f8eb7b3f0a5c8529c4a04c4d9755283cac6a15
Kévin Le Torc'h <kev29lt@gmail.com>
2020-09-03 22:58:56 +0200

    Load config from package by default

--- fetch_cord/config.py
+++ fetch_cord/config.py
@@ -4,32 +4,52 @@ from pathlib import Path
 import copy
 import json
 import configparser
+try:
+    import importlib.resources as pkg_resources
+except ImportError:
+    # Try backported to PY<37 `importlib_resources`.
+    import importlib_resources as pkg_resources
+import fetch_cord.ressources as ressources
 
 class ConfigError(Exception):
     pass
 
-
 def load_config():
+    default_config = configparser.ConfigParser()
+    with pkg_resources.path(ressources, 'fetch_cord.conf') as path:
+        default_config.read_file(open(path))
+        default_config.read(['/etc/fetch_cord.conf', os.path.expanduser('~/fetch_cord.conf')])
 
-    base_config = configparser.ConfigParser()
-    base_config.read("/etc/fetch_cord.conf")
-    base_config = _parsed_config_to_dict(base_config)
-    _validate_config(base_config)
+        for item in default_config.items():
+            print(item)
+
+        default_config = _parsed_config_to_dict(default_config)
+        _validate_config(default_config)
+
+        return default_config
 
 
-#    try:
-    user_config = configparser.ConfigParser()
-    user_config.read("/etc/fetch_cord.conf")
-    user_config = _parsed_config_to_dict(user_config)
-#    except configparser.ParsingError as e:
-#        print(
-#            "Error parsing config file %s. Falling back to default config %s. Error is : %s",
-#            envs.USER_CONFIG_COPY_PATH, envs.DEFAULT_CONFIG_PATH, str(e))
-    return base_config
+# def load_config():
+#     base_config = configparser.ConfigParser()
+#     base_config.read(["/etc/fetch_cord.conf"])
+#     base_config = _parsed_config_to_dict(base_config)
+#     _validate_config(base_config)
 
-    corrected_config = _validate_config(user_config, fallback_config=base_config)
 
-    return corrected_config
+# #    try:
+#     user_config = configparser.ConfigParser()
+#     user_config.read("/etc/fetch_cord.conf")
+#     user_config = _parsed_config_to_dict(user_config)
+# #    except configparser.ParsingError as e:
+# #        print(
+# #            "Error parsing config file %s. Falling back to default config %s. Error is : %s",
+# #            envs.USER_CONFIG_COPY_PATH, envs.DEFAULT_CONFIG_PATH, str(e))
+#     return base_config
+
+#     corrected_config = _validate_config(user_config, fallback_config=base_config)
+
+
+#     return corrected_config
 
 def _validate_config(config, fallback_config=None):
 
