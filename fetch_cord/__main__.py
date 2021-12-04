fa7c2c59548d373d56e32a36a380ded1784eab8b
Kévin Le Torc'h <kev29lt@gmail.com>
2021-12-04 22:24:35 +0100

    Import gui stuff only when used

--- fetch_cord/__main__.py
+++ fetch_cord/__main__.py
@@ -9,15 +9,17 @@ from .debugger import run_rpc_debug
 from .update import update
 from . import __init__ as __init__
 from .resources import systemd_service
-from .gui import gui
+
+args = parse_args()
+
+if args.gui:
+    from .gui import gui
 
 
 def main():
-    args = parse_args()
 
     if args.gui:
         gui.main()
-        sys.exit(0)
 
     if args.update:
         update()
