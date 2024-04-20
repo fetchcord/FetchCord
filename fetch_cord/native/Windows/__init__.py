012b1313110398a2de52d681a23af7a1a53ff4b6
BlivionIaG <kev29lt@gmail.com>
2024-04-21 00:50:11 +0200

    Linux specific fixes
    Disable import of windows libs on non windows systems
    Fix host/motherboard dection on linux

--- fetch_cord/native/Windows/__init__.py
+++ fetch_cord/native/Windows/__init__.py
@@ -1,12 +1,18 @@
-from typing import Optional
-from . import packages
+import os
 
-fetch_map = {"packages": packages}
+if os.name == "nt":
+    from typing import Optional
+    from . import packages
 
+    def fetch(component_class: str) -> Optional[str | None]:
+        fetch_map = {"packages": packages}
 
-def fetch(component_class: str) -> Optional[str | None]:
+        if component_class not in fetch_map:
+            return None
 
-    if component_class not in fetch_map:
-        return None
+        return fetch_map[component_class].fetch()
 
-    return fetch_map[component_class].fetch()
+else:
+
+    def fetch(component_class: str) -> None:
+        return "Not implemented for this platform."
