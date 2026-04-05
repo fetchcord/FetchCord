d39eb59f12df630b78db650ff89929c54751ad44
BlivionIaG <kev29lt@gmail.com>
2026-04-05 23:34:36 +0200

    update: Bump minimum Python version to 3.10
    As of April 2026, Python 3.10 is the oldest supported version.
    - 3.7 EOL: June 2023
    - 3.8 EOL: October 2024
    - 3.9 EOL: October 2025
    - 3.10 supported until October 2026
    
    Refs: fetchcord-testing-fixes

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.10",
     entry_points={
         "console_scripts": [
             "fetchcord=fetch_cord.__main__:main",
